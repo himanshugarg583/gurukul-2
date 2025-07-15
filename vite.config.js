68e63ced22ced392feb3189ac8dc960f7933cb29
himanshugarg583 <himanshu.garg@fonixtech.io>
2025-07-15 11:54:21 +0530

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-   base: '/gurukul-2/',
-  plugins: [react(),tailwindcss()],
+  base: '/', // ✅ Fix this line
+  plugins: [react(), tailwindcss()],
 })
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// // import tailwindcss from '@tailwindcss/vite'
-
-// export default defineConfig({
-//   base: '/gurukul/', // 👈 MUST match your GitHub repo name
-//   plugins: [react()],
-// })
